51e6f6152b0018fd2bec0cd2e66cdbb2b636d87e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2023-11-22 12:54:34 -0800

    updated

--- sort.h
+++ sort.h
@@ -26,11 +26,19 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 /* Helper Function Prototypes */
 void node_swap(listint_t **list, listint_t *pos, listint_t *node);
 void q_sort(int *array, size_t size, int start, int end);
 int lomuto_part(int *array, size_t size, int start, int end);
 void list_swap(listint_t **list, listint_t *left_n, listint_t *right_n);
+void hq_sort(int *array, size_t size, int start, int end);
+int hoare_part(int *array, size_t size, int start, int end);
 
 #endif /* SORT_H */
